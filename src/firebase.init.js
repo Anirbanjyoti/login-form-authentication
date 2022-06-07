d4e4732608f6a81598f0fb574e7a71792817b05d
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-07 10:55:27 +0530

    form Design

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8YZAxMM_wKzkWOhLpPl2Nip5sjnE2Xt8",
+  authDomain: "login-form-authenticatio-86f58.firebaseapp.com",
+  projectId: "login-form-authenticatio-86f58",
+  storageBucket: "login-form-authenticatio-86f58.appspot.com",
+  messagingSenderId: "328540099610",
+  appId: "1:328540099610:web:a734ae269b98002b866da3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
